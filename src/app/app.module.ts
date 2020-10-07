831cf41fca6fe5d16c35b1946333d30a896aac6d
Remi <42407006+GuysmoB@users.noreply.github.com>
2020-10-07 17:39:43 +0200

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgApexchartsModule } from "ng-apexcharts";
 
+import { FusionChartsModule } from 'angular-fusioncharts';
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import * as TimeSeries from 'fusioncharts/fusioncharts.timeseries';
+import * as Excel from 'fusioncharts/fusioncharts.excelexport';
+
+FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme, TimeSeries, Excel);
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +21,8 @@ import { NgApexchartsModule } from "ng-apexcharts";
   imports: [
     HttpClientModule,
     BrowserModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    FusionChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
